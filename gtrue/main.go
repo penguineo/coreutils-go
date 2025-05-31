318c3a6cad56424334ea367e24bf27acf91232d0
penguineo <subham020304@gmail.com>
2025-05-31 15:02:46 +0530

    feat: add --help flag

--- gtrue/main.go
+++ gtrue/main.go
@@ -8,6 +8,18 @@ import (
 func main() {
 	for _, args := range os.Args[1:] {
 		switch args {
+		case "--help":
+			fmt.Println(
+				"\n\ngtrue [ignored command line arguements]\n",
+				"gtrue OPTION\n\n",
+				"Description:\n",
+				"\tExit with a status code indicating success.\n\n",
+				"--help display this help and exit\n\n",
+				"--version output version information and exit\n\n",
+				"Your shell may have its own version of true, which usually supersedes the version described here.",
+				"Please refer to your shell's documentation for details about options it supports.",
+			)
+			os.Exit(0)
 		default:
 			os.Exit(0)
 		}
